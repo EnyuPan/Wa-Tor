aa69214da850997516938f58a5a294d1a73c0d13
Enyu Pan <101580450@georgebrown.ca>
2024-12-13 14:55:58 -0500

    Add grid to window_test.py

--- window_test.py
+++ window_test.py
@@ -1,13 +1,22 @@
 import pyglet
 from pyglet import window, app, graphics, shapes
+import random
 w = window.Window()
 
-batch = graphics.Batch()
-rectangle = shapes.Rectangle(0, 10, 100, 100, color=(0, 255, 0), batch=batch)
+b = graphics.Batch()
+
+rows = 10
+cols = 10
+cells = [None] * rows * cols
+cell_width = w.width / rows
+cell_height = w.height / cols
+print(cell_width, cell_height)
+for i in range(rows):
+    for j in range(cols):
+        cells[i * rows + j] = shapes.Rectangle(i * cell_width, j * cell_height, cell_width, cell_height, color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), batch=b)
 
 @w.event
 def on_draw():
-    w.clear()
-    batch.draw()
+    b.draw()
 
 app.run()
