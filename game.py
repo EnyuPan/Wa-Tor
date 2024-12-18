42d7917ca00918256505d977ad923fbada818ac0
EnyuPan <eric.enyu.pan@gmail.com>
2024-12-17 22:34:00 -0500

    Add options to pause or run game; edit Game class and testing harness

--- game.py
+++ game.py
@@ -2,36 +2,49 @@ from grid import Grid
 from enum import Enum
 
 class Commands(Enum):
-    INIT_GRID = 1 # requires 2 arguments, representing the numbers of rows and cols
-    SET_CELL = 2 # requires 3 arguments, representing i, j, and creature type
-    POPULATE = 3 # requires 2 arguments, representing the numbers of fish and sharks
-    TICK = 4 # requires no arguments
-    RESET_GRID = 5 # requires no arguments
-    QUIT = 6 # requires no arguments
+    INIT_GRID = 1 # requires 2 arguments, representing the numbers of rows and cols; requires self is active
+    SET_CELL = 2 # requires 3 arguments, representing i, j, and creature type; requires self is active
+    POPULATE = 3 # requires 2 arguments, representing the numbers of fish and sharks; requires self is active
+    TICK = 4 # requires self is active
+    RESET_GRID = 5 # requires self is active
+    RUN = 6
+    PAUSE = 7
+    QUIT = 8
 
 class Game:
     def __init__(self, grid: Grid=None):
         self.grid = grid
-        self.running = False
+        self.running = True
+        self.active = False # True if the game is accepting commands; False if the game is paused
     
     def run(self):
         self.running = True
+        self.active = True
+    
+    def pause(self):
+        self.active = False
     
     def quit(self):
         self.running = False
+        self.active = False
 
     def process_input(self, *args):
-        if not(self.running):
-            return
         if args[0] == Commands.INIT_GRID:
             self.grid = Grid(args[1], args[2])
-        elif args[0] == Commands.SET_CELL:
-            self.grid.set_cell(args[1], args[2], args[3])
-        elif args[0] == Commands.POPULATE:
-            self.grid.populate(args[1], args[2])
-        elif args[0] == Commands.TICK:
-            self.grid.tick()
-        elif args[0] == Commands.RESET_GRID:
-            self.grid.reset()
+        elif args[0] == Commands.RUN:
+            self.run()
+        elif args[0] == Commands.PAUSE:
+            self.pause()
         elif args[0] == Commands.QUIT:
             self.quit()
+        elif self.active:
+            if args[0] == Commands.SET_CELL:
+                self.grid.set_cell(args[1], args[2], args[3])
+            elif args[0] == Commands.POPULATE:
+                self.grid.populate(args[1], args[2])
+            elif args[0] == Commands.TICK:
+                self.grid.tick()
+            elif args[0] == Commands.RESET_GRID:
+                self.grid.reset()
+        else:
+            print("Game paused - enter X to resume")
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from game import Game, Commands
 g = Game()
 g.run()
 g.process_input(Commands.INIT_GRID, int(input("Enter number of rows: ")), int(input("Enter number of cols: ")))
-print(g.grid)
 s = ""
 while g.running:
-    s = input("Enter command (F: add fish, S: add shark, T: tick, R: reset, P: populate, Q: quit): ").lower()
+    if g.active:
+        print(g.grid)
+    s = input("Enter command (X: run, Z: pause, F: add fish, S: add shark, T: tick, R: reset, P: populate, Q: quit): ").lower()
     if s == "q":
-        g.quit()
+        g.process_input(Commands.QUIT)
+    elif s == "z":
+        g.process_input(Commands.PAUSE)
+    elif s == "x":
+        g.process_input(Commands.RUN)
     elif s == "f":
         g.process_input(Commands.SET_CELL, int(input("row: ")), int(input("col: ")), "fish")
     elif s == "s":
@@ -19,4 +24,3 @@ while g.running:
         g.process_input(Commands.TICK)
     elif s == "p":
         g.process_input(Commands.POPULATE, int(input("number of fish: ")), int(input("number of sharks: ")))
-    print(g.grid)
