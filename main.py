75db765d5a5a01a1b7ba65369729fbc51db255fc
EnyuPan <eric.enyu.pan@gmail.com>
2024-12-30 06:32:09 -0500

    Edit main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import game_interface
 
 print("=== WA-TOR SIMULATION ===")
 while True:
-    s = input("Select interface (C: command line, G: graphical): ").lower()
+    s = input("Select interface (C: command line, G: graphical): ").strip().lower()
     if (s == 'c'):
         itf = game_interface.CommandInterface()
         break
     elif (s == 'g'):
         itf = game_interface.GraphicalInterface()
         break
+    elif (s == 'q'):
+        exit()
     else:
-        print("Invalid input")
+        print("Interface not recognized. Please try again.")
 
 itf.run()
